6749a46f407669afe035b286db62f6ad533ca47c
D-coder22 <amitnmf@gmail.com>
2020-10-10 19:47:51 +0530

    Switched from pytorch to keras

--- classifiers/pneumonia-classifier/generate_configs.py
+++ classifiers/pneumonia-classifier/generate_configs.py
@@ -1,8 +1,10 @@
 import os
 
-import torch
-from torch import nn
-import torch.nn.functional as F 
+import keras
+from keras import backend as K
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Dense, Dropout, Flatten
+from keras.models import Sequential
 
 import classifiers.datahandlers as datahandlers
 
@@ -25,25 +27,27 @@ def get_local_training_config():
 
 def get_hyperparams():
     hyperparams = {
-        'gobal': {
-            'rounds': 3,
+        'global': {
+            'rounds': 10,
             'termination_accuracy': 0.9,
             'max_timeout': 60
         },
         'local': {
-            'trainging': {
+            'training': {
                 'epochs': 5
             },
             'optimizer': {
-                'lr': 0.002
+                'lr': 0.01
             }
         }
     }
 
+    return hyperparams
+
 
 def get_data_handler_config(party_id, dataset, folder_data, is_agg=False):
 
-    SUPPORTED_DATASETS = ['chest-xray']
+    SUPPORTED_DATASETS = ['mnist']
     if dataset in SUPPORTED_DATASETS:
         data = datahandlers.get_datahandler_config(
             dataset, folder_data, party_id, is_agg)
@@ -53,68 +57,57 @@ def get_data_handler_config(party_id, dataset, folder_data, is_agg=False):
     return data
 
 
-class Network(nn.Module()):
-    def __init__(self):
-        super(self, Network).__init__()
-
-        self.conv1 = nn.Conv2d(3, 32, kernal_size = 3, stride = 1, padding = 0)
-        self.conv2 = nn.Conv2d(32, 32, kernal_size = 3, stride = 1, padding = 0)
-        self.conv3 = nn.Conv2d(32, 64, kernal_size = 3, stride = 1, padding = 0)
-        self.conv4 = nn.Conv2d(64, 64, kernal_size = 3, stride = 1, padding = 0)
-        self.conv5 = nn.Conv2d(64, 128, kernal_size = 3, stride = 2, padding = 0)
-        self.conv6 = nn.Conv2d(128, 128, kernal_size = 3, stride = 2, padding = 0)
-        self.pool = nn.MaxPool2d(2,2)
-        self.fc1 = nn.Linear(4608, 1152)
-        self.fc2 = nn.Linear(1152, 288)
-        self.fc3 = nn.Linear(288, 2)
-        self.dropout = nn.Dropout(0.3)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = self.pool(F.relu(self.conv2(x)))
-        
-        x = F.relu(self.conv3(x))
-        x = self.pool(F.relu(self.conv4(x)))
-
-        x = self.pool(F.relu(self.conv5(x)))
-
-        x = self.pool(F.relu(self.conv6(x)))
-
-        x = x.view(-1, 4608)
-        x = self.dropout(F.relu(self.fc1(x)))
-        x = self.dropout(F.relu(self.fc2(x)))
-        x = F.log_softmax(self.fc1(x))
-
-        return x
-
-def get_model_config(folder_configs, dataset, is_agg = False, party_id = 0):
+def get_model_config(folder_configs, dataset, is_agg=False, party_id=0):
     if is_agg:
         return None
 
-    model = Network().cuda()
+    num_classes = 2
+    img_rows, img_cols = 500, 500
+    if K.image_data_format() == 'channels_first':
+        input_shape = (1, img_rows, img_cols)
+    else:
+        input_shape = (img_rows, img_cols, 1)
+
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
+                     input_shape=input_shape))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
 
     if not os.path.exists(folder_configs):
         os.makedirs(folder_configs)
 
-    fname = os.path.join(folder_configs, 'complied_torch.h5')
-    torch.save(model, fname)
+    # Save model
+    fname = os.path.join(folder_configs, 'compiled_keras.h5')
+    model.save(fname)
 
-    torch.cuda.empty_cache()
-
-
+    K.clear_session()
+    # Generate model spec:
     spec = {
-        'model_name': 'pytorch-cnn',
+        'model_name': 'keras-cnn',
         'model_definition': fname
     }
 
     model = {
-        'name': 'PytorchFLModel',
-        'path': 'ibmfl.model.pytorch_fl_model',
+        'name': 'KerasFLModel',
+        'path': 'ibmfl.model.keras_fl_model',
         'spec': spec
     }
 
     return model
-
-
-
-
