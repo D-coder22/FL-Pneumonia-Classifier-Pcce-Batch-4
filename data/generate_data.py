b3232d683d857dd6198a6c00606c7ca5ba82bf54
D-coder22 <amitnmf@gmail.com>
2020-10-12 17:45:24 +0530

    Random data generation finalized

--- data/generate_data.py
+++ data/generate_data.py
@@ -26,24 +26,24 @@ test_path1 = os.path.join(fl_path, "chest_xray", "train", "PNEUMONIA")
 images_test0 = glob(os.path.join(test_path0, "*.jpeg"))
 images_test1 = glob(os.path.join(test_path1, "*.jpeg"))
 
-WIDTH = 224
-HEIGHT = 224
+WIDTH = 150
+HEIGHT = 150
 
 x_train = []
 y_train = []
 x_test = []
 y_test = []
-# for i in range(0,199):
-#     if i%2==0:
-#         img = cv2.imread(random.sample(images0,1)[0])
+for i in range(0,199):
+    if i%2==0:
+        img = cv2.imread(random.sample(images0,1)[0])
 
-#         x_train.append(cv2.resize(img, (WIDTH,HEIGHT), interpolation=cv2.INTER_CUBIC))
-#         y_train.append(0)
-#     else:
-#         img = cv2.imread(random.sample(images1,1)[0])
+        x_train.append(cv2.resize(img, (WIDTH,HEIGHT), interpolation=cv2.INTER_CUBIC))
+        y_train.append(0)
+    else:
+        img = cv2.imread(random.sample(images1,1)[0])
 
-#         x_train.append(cv2.resize(img, (WIDTH,HEIGHT), interpolation=cv2.INTER_CUBIC))
-#         y_train.append(1)
+        x_train.append(cv2.resize(img, (WIDTH,HEIGHT), interpolation=cv2.INTER_CUBIC))
+        y_train.append(1)
 
 for i in range(1,5000):
     if i%2==0:
@@ -57,10 +57,12 @@ for i in range(1,5000):
         x_test.append(cv2.resize(img, (WIDTH,HEIGHT), interpolation=cv2.INTER_CUBIC))
         y_test.append(1)
 
-# name_file = 'data_party' + str(0) + '.npz'
-# name_file = os.path.join(fl_path, name_file)
-# np.savez(name_file, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
+name_file = 'data_party' + str(0) + '.npz'
+name_file = os.path.join(fl_path, name_file)
+np.savez(name_file, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
 
+x_train = []
+y_train = []
 for i in range(0,199):
     if i%3==0:
         img = cv2.imread(random.sample(images0,1)[0])
@@ -77,6 +79,8 @@ name_file = 'data_party' + str(1) + '.npz'
 name_file = os.path.join(fl_path, name_file)
 np.savez(name_file, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
 
+x_train = []
+y_train = []
 for i in range(0,199):
     if i%2==0:
         img = cv2.imread(random.sample(images0,1)[0])
